7fd822a93dcb3290dd42d40dc4ab0a2bb764e883
Sanidhya Suman <sanidhyasuman23@gmail.com>
2019-08-19 19:40:41 +0530

    Modified main function for calculator

--- main.c
+++ main.c
@@ -4,7 +4,6 @@ int main(int argc, char *argv[])
 {
 	char *str, *temp;
 	struct node_info *list;
-//	str = "1+2*10-4";
 	if(argc == 1)
 	{
 		str = (char *)malloc(100);
@@ -12,7 +11,9 @@ int main(int argc, char *argv[])
 		temp = str;
 	}
 	else
+	{
 		temp = argv[1];
+	}
 	printf("%s = ",temp);
 	list = infix_to_postfix(temp);
 	float result = eval_postfix(list);
